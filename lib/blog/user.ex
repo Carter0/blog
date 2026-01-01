b229b6488cf69cc2a1ebe5a6b8ddbf84893a112e
Carter Weinberg <weinbergcarter@gmail.com>
2025-12-31 21:02:58 -0500

    Made a basic backend

--- lib/blog/user.ex
+++ lib/blog/user.ex
@@ -1,13 +1,17 @@
 defmodule Blog.User do
   use Ecto.Schema
+
   import Ecto.Changeset
+  import Ecto.Query
+
+  alias Blog.{Repo, User}
 
   @type t :: %__MODULE__{
-          id: integer(),
-          username: String.t(),
-          email: String.t(),
-          inserted_at: NaiveDateTime.t(),
-          updated_at: NaiveDateTime.t()
+          id: integer() | nil,
+          username: String.t() | nil,
+          email: String.t() | nil,
+          inserted_at: NaiveDateTime.t() | nil,
+          updated_at: NaiveDateTime.t() | nil
         }
 
   schema "users" do
@@ -24,5 +28,19 @@ defmodule Blog.User do
     user
     |> cast(attrs, [:username, :password, :email])
     |> validate_required([:username, :password, :email])
+  end
+
+  @spec create_user(map()) :: {:ok, t()} | {:error, Ecto.Changeset.t()}
+  def create_user(user_attrs) do
+    %User{}
+    |> User.changeset(user_attrs)
+    |> Repo.insert()
+  end
+
+  @spec login(String.t(), String.t()) :: boolean()
+  def login(username, password) do
+    User
+    |> where([u], u.username == ^username and u.password == ^password)
+    |> Repo.exists?()
   end
 end
